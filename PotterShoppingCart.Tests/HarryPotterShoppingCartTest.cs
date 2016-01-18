7fd8f8c35db58df72c624cbfac4988f9ee48b58b
jasonten <chenten1@gmail.com>
2016-01-18 11:35:10 +0800

    建立測試程式
    建立測試程式

---
+++ PotterShoppingCart.Tests/HarryPotterShoppingCartTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass()]
+    public class HarryPotterShoppingCartTest
+    {
+        [TestMethod()]
+        public void ShoppingCartTest_buy_BookName_1_BookCount_1_Total_100()
+        {
+            List<BookList> BookLists = new List<BookList>()
+            {
+             new  BookList(){ BookName = "HarryPotter1",BookCount = 1}
+            };
+            int price = 100;
+
+        }
+    }
+    public class BookList
+    {
+        public string BookName { get; set; }
+        public int BookCount { get; set; }
+    }
+}
